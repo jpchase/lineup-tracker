d6e293481099d21421f632b2613357ed1d78690b
Jason Chase <jasonpchase@gmail.com>
2016-05-19 17:38:54 -0400

    Initial commit

---
+++ app/scripts/live-game.js
@@ -0,0 +1,60 @@
+/*!
+ *
+ *  Web Starter Kit
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+/* eslint-env browser */
+(function() {
+  'use strict';
+
+  // Your custom JavaScript goes here
+  var liveGame = {
+    isLoading: true,
+    gameId: null,
+//    spinner: document.querySelector('.loader'),
+//    cardTemplate: document.querySelector('.cardTemplate'),
+//    container: document.querySelector('.main'),
+//    addDialog: document.querySelector('.dialog-container'),
+  };
+
+  liveGame.gameId = window.localStorage.liveGameId;
+
+  if (!liveGame.gameId) {
+    var pos = window.location.search.indexOf('?game=');
+    if (pos > -1) {
+      var gameId = window.location.search.substr(pos + '?game='.length);
+      liveGame.gameId = gameId;
+    }
+  }
+
+  console.log('Starting game: ', liveGame.gameId);
+
+  /*****************************************************************************
+   *
+   * Event listeners for UI elements
+   *
+   ****************************************************************************/
+
+  document.getElementById('start').addEventListener('click', function() {
+    // Refresh all of the forecasts
+    liveGame.updateForecasts();
+  });
+
+  document.getElementById('butAdd').addEventListener('click', function() {
+    // Open/show the add new city dialog
+    liveGame.toggleAddDialog(true);
+  });
+})();
