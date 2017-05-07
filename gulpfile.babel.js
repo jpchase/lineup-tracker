82bb00e45b708c550fe8feea2803f867fd0bd2c1
Jason Chase <jasonpchase@gmail.com>
2017-05-07 11:07:56 -0400

    Initial integration of Polymer for roster

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -38,9 +38,15 @@ import source from 'vinyl-source-stream';
 import {output as pagespeed} from 'psi';
 import pkg from './package.json';
 import merge from 'merge-stream';
+import polymerJson from './polymer.json';
+//import polymerBuild from 'polymer-build';
+//console.log('polymerBuild', polymerBuild);
+const PolymerProject = require('polymer-build').PolymerProject;
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
+
+const project = new PolymerProject(polymerJson);
 
 // Lint JavaScript
 gulp.task('lint', () =>
@@ -208,13 +214,23 @@ gulp.task('html', () => {
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('polymer', () => {
+  merge(project.sources(), project.dependencies())
+  .pipe($.rename((path) => {
+                             if (path.dirname.startsWith('src'))
+                               path.dirname = path.dirname.substring(3);
+                           }))
+ .pipe(gulp.dest('.tmp/'));
+
+});
+
 // Clean output directory
 gulp.task('clean', () => {
   del(['.tmp', 'dist/*', '!dist/.git'], {dot: true});
 });
 
 // Watch files for changes & reload
-gulp.task('serve', ['scripts', 'styles'], () => {
+gulp.task('serve', ['scripts', 'styles', 'polymer'], () => {
   browserSync({
     notify: false,
     // Customize the Browsersync console logging prefix
@@ -230,6 +246,7 @@ gulp.task('serve', ['scripts', 'styles'], () => {
   });
 
   gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['src/**/*.html'], reload);
   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
   gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts']);
   gulp.watch(['app/images/**/*'], reload);
