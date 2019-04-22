2162d3ae8530b299a2b54f1778b74f32adca6b83
Jason Chase <jasonpchase@gmail.com>
2019-04-22 18:41:42 -0400

    Fix roster view to reload roster on team change

--- src/components/lineup-roster.ts
+++ src/components/lineup-roster.ts
@@ -3,21 +3,8 @@
 */
 
 import { LitElement, html, property } from 'lit-element';
-import { connect } from 'pwa-helpers/connect-mixin.js';
 
 import { Roster } from '../models/team.js';
-
-// This element is connected to the Redux store.
-import { store, RootState } from '../store.js';
-
-// We are lazy loading its reducer.
-import team from '../reducers/team.js';
-store.addReducers({
-  team
-});
-
-// These are the actions needed by this element.
-import { getRoster } from '../actions/team.js';
 
 // These are the elements needed by this element.
 import './lineup-roster-item.js';
@@ -25,15 +12,16 @@ import './lineup-roster-item.js';
 // These are the shared styles needed by this element.
 import { SharedStyles } from './shared-styles.js';
 
-class LineupRoster extends connect(store)(LitElement) {
+// This element is *not* connected to the Redux store.
+class LineupRoster extends LitElement {
   protected render() {
     return html`
       ${SharedStyles}
       <style>
         :host { display: block; }
       </style>
-      ${Object.keys(this._roster).map((key) => {
-        const player = this._roster[key];
+      ${Object.keys(this.roster).map((key) => {
+        const player = this.roster[key];
         return html`
           <div>
             <lineup-roster-item .isGame="false" .player="${player}"></lineup-roster-item>
@@ -43,17 +31,8 @@ class LineupRoster extends connect(store)(LitElement) {
     `;
   }
 
-  @property({type: Object})
-  private _roster: Roster = {};
-
-  protected firstUpdated() {
-    store.dispatch(getRoster());
-  }
-
-  // This is called every time something is updated in the store.
-  stateChanged(state: RootState) {
-    this._roster = state.team!.roster;
-  }
+  @property({ type: Object })
+  roster: Roster = {};
 }
 
 window.customElements.define('lineup-roster', LineupRoster);
--- src/components/lineup-view-roster.ts
+++ src/components/lineup-view-roster.ts
@@ -5,17 +5,21 @@
 import { html, property } from 'lit-element';
 import { PageViewElement } from './page-view-element.js';
 
-// import { Team } from '../models/team.js';
+import { Roster } from '../models/team.js';
 
 // This element is connected to the Redux store.
 import { connect } from 'pwa-helpers/connect-mixin.js';
 import { store, RootState } from '../store.js';
+import { TeamState } from '../reducers/team.js';
 
 // We are lazy loading its reducer.
 import team from '../reducers/team.js';
 store.addReducers({
     team
 });
+
+// These are the actions needed by this element.
+import { getRoster } from '../actions/team.js';
 
 // These are the elements needed by this element.
 import './lineup-roster.js';
@@ -29,17 +33,32 @@ class LineupViewRoster extends connect(store)(PageViewElement) {
       ${SharedStyles}
       <section>
         <h2>Team: ${this._teamName}</h2>
-        <lineup-roster></lineup-roster>
+        <lineup-roster .roster="${this._roster}"></lineup-roster>
       </section>
     `;
   }
 
   @property({ type: String })
+  private _teamId = '';
+
+  @property({ type: String })
   private _teamName: string = '';
+
+  @property({ type: Object })
+  private _roster: Roster = {};
 
   // This is called every time something is updated in the store.
   stateChanged(state: RootState) {
-    this._teamName = state.team!.teamName;
+    if (!state.team) {
+      return;
+    }
+    const teamState: TeamState = state.team!;
+    if (this._teamId !== teamState.teamId) {
+      this._teamId = teamState.teamId;
+      store.dispatch(getRoster(this._teamId));
+    }
+    this._teamName = teamState.teamName;
+    this._roster = teamState.roster;
   }
 
 }
