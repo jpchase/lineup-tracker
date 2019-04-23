1d096082e45f650ac04adeed0ecee33008a35a7a
Jason Chase <jasonpchase@gmail.com>
2019-04-23 00:06:06 -0400

    Add placeholder element when roster has no players

--- src/components/lineup-roster.ts
+++ src/components/lineup-roster.ts
@@ -15,20 +15,34 @@ import { SharedStyles } from './shared-styles.js';
 // This element is *not* connected to the Redux store.
 class LineupRoster extends LitElement {
   protected render() {
+    const roster = this.roster;
+    const playerList = roster ? Object.keys(roster).map(key => roster[key]) : [];
     return html`
       ${SharedStyles}
       <style>
         :host { display: block; }
+        .empty-roster {
+          text-align: center;
+          white-space: nowrap;
+          color: var(--app-secondary-color);
+        }
       </style>
-      ${Object.keys(this.roster).map((key) => {
-        const player = this.roster[key];
-        return html`
+      <div>
+      ${playerList.length > 0 ? html`
+        <div class="list">
+        ${playerList.map(player => html`
           <div>
-            <lineup-roster-item .isGame="false" .player="${player}"></lineup-roster-item>
+            <lineup-roster-item .isGame="false" .player="${player}">
+            </lineup-roster-item>
           </div>
-        `
-      })}
-    `;
+        `)}
+        </div>
+      ` : html`
+        <p class="empty-roster">
+          No players in roster.
+        </p>
+      `}
+      </div>`
   }
 
   @property({ type: Object })
