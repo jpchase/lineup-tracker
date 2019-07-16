2e7762612df497b3f6d68bb5c4f265def8c012d6
Jason Chase <jasonpchase@gmail.com>
2019-07-16 19:55:10 -0400

    Use a PlayerStatus enum instead of bare strings

--- src/models/player.ts
+++ src/models/player.ts
@@ -2,12 +2,19 @@
 @license
 */
 
+export enum PlayerStatus {
+  Next = 'NEXT',
+  Off = 'OFF',
+  On = 'ON',
+  Out = 'OUT'
+}
+
 export interface Player {
   id: string;
   name: string;
   uniformNumber: number;
   positions: string[];
-  status: string;
+  status: PlayerStatus;
 }
 
 export interface Roster {
