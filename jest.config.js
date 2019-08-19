165200277d44e567a051b121fbf766919c132599
Jason Chase <jasonpchase@gmail.com>
2019-08-19 00:50:40 -0400

    Exclude generated JS test files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-babel',
   testEnvironment: 'jsdom',
+  testMatch: [ '**/?(*.)+(spec|test).ts?(x)' ],
   globals: {
     'ts-jest': {
       diagnostics: {
