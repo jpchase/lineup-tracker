2a6706822e54959a56996410347b09920370e4fc
Jason Chase <jasonpchase@gmail.com>
2019-11-02 23:31:24 -0400

    Convert auth action tests to Karma

--- test/actions/auth.test.ts
+++ test/actions/auth.test.ts
@@ -1,17 +1,17 @@
 import * as actions from '@app/actions/auth';
-import * as actionTypes from '@app/slices/auth-types';
-import { User } from '@app/models/auth';
 import { authRef /*, provider */ } from "@app/firebase";
+import { User } from '@app/models/auth';
+import * as actionTypes from '@app/slices/auth-types';
 import { Error as FirebaseError, User as FirebaseUser, UserCredential } from '@firebase/auth-types';
 import { Observer, Unsubscribe } from '@firebase/util';
+import { expect } from '@open-wc/testing';
+import * as sinon from 'sinon';
 
 type AuthStateChangedFn = (
   nextOrObserver: Observer<any> | ((a: FirebaseUser | null) => any),
     error ?: (a: FirebaseError) => any,
     completed ?: Unsubscribe
   ) => Unsubscribe;
-
-jest.mock('@app/firebase');
 
 function buildFirebaseUser(uid: number): FirebaseUser {
   const result: any = {
@@ -29,19 +29,23 @@ function buildUserCredential(user: FirebaseUser): UserCredential {
 }
 
 describe('getUser', () => {
-  let changedSpy = jest.spyOn(authRef, 'onAuthStateChanged');
+  let changedSpy: sinon.SinonStub;
+
+  beforeEach(() => {
+    changedSpy = sinon.stub(authRef, 'onAuthStateChanged');
+  });
 
   afterEach(() => {
-    changedSpy.mockRestore();
+    sinon.restore();
   });
 
   it('should return a function to dispatch the getUser action', () => {
-    expect(typeof actions.getUser()).toBe('function');
+    expect(actions.getUser()).to.be.instanceof(Function);
   });
 
   it('should dispatch an action to get user after sign in', () => {
-    const dispatchMock = jest.fn();
-    const getStateMock = jest.fn();
+    const dispatchMock = sinon.stub();
+    const getStateMock = sinon.stub();
 
     const firebaseUser = {
       uid: 'su1', displayName: 'Signed in user 1',
@@ -56,27 +60,27 @@ describe('getUser', () => {
       return {} as Unsubscribe;
     };
 
-    changedSpy.mockImplementation(changedMock);
+    changedSpy.onFirstCall().callsFake(changedMock);
 
     actions.getUser()(dispatchMock, getStateMock, undefined);
 
     // Checks that onAuthStateChanged() was called with a callback.
-    expect(changedSpy.mock.calls.length).toBe(1);
-    expect(typeof changedSpy.mock.calls[0][0]).toBe('function');
+    expect(changedSpy).to.have.callCount(1);
+    expect(changedSpy.firstCall).to.have.been.calledWith(sinon.match.func);
 
     const signedInUser: User = {
       id: 'su1', name: 'Signed in user 1', email: '', imageUrl: ''
     };
 
-    expect(dispatchMock).toBeCalledWith(expect.objectContaining({
+    expect(dispatchMock).to.have.been.calledWith({
       type: actionTypes.GET_USER_SUCCESS,
       user: signedInUser,
-    }));
+    });
   });
 
   it('should dispatch an action with no user after sign out', () => {
-    const dispatchMock = jest.fn();
-    const getStateMock = jest.fn();
+    const dispatchMock = sinon.stub();
+    const getStateMock = sinon.stub();
 
     const changedMock: AuthStateChangedFn = (nextOrObserver /*, error, completed*/): Unsubscribe => {
       if (typeof nextOrObserver === 'function') {
@@ -85,59 +89,61 @@ describe('getUser', () => {
       return {} as Unsubscribe;
     };
 
-    changedSpy.mockImplementation(changedMock);
+    changedSpy.onFirstCall().callsFake(changedMock);
 
     actions.getUser()(dispatchMock, getStateMock, undefined);
 
     // Checks that onAuthStateChanged() was called with a callback.
-    expect(changedSpy.mock.calls.length).toBe(1);
+    expect(changedSpy).to.have.callCount(1);
 
-    expect(dispatchMock).toBeCalledWith(expect.objectContaining({
+    expect(dispatchMock).to.have.been.calledWith({
       type: actionTypes.GET_USER_SUCCESS,
       user: {},
-    }));
+    });
   });
 
 });
 
 describe('signIn', () => {
-  const signInSpy = jest.spyOn(authRef, 'signInWithPopup');
-  const logSpy = jest.spyOn(global.console, 'log');
-  const errorSpy = jest.spyOn(global.console, 'error');
+  let signInSpy: sinon.SinonStub;
+
+  beforeEach(() => {
+    signInSpy = sinon.stub(authRef, 'signInWithPopup');
+    sinon.stub(console, 'log');
+    sinon.stub(console, 'error');
+  });
 
   afterEach(() => {
-    signInSpy.mockRestore();
-    logSpy.mockRestore();
-    errorSpy.mockRestore();
+    sinon.restore();
   });
 
   it('should return a function to do the sign in', () => {
-    expect(typeof actions.signIn()).toBe('function');
+    expect(actions.signIn()).to.be.instanceof(Function);
   });
 
   it('should call the Firebase sign in method and log success', () => {
-    const dispatchMock = jest.fn();
-    const getStateMock = jest.fn();
+    const dispatchMock = sinon.stub();
+    const getStateMock = sinon.stub();
 
     const result: UserCredential = buildUserCredential(buildFirebaseUser(1234));
-    signInSpy.mockResolvedValue(Promise.resolve(result));
+    signInSpy.onFirstCall().resolves(result);
 
     actions.signIn()(dispatchMock, getStateMock, undefined);
 
     // Checks that signInWithPopup() was called.
-    expect(signInSpy.mock.calls.length).toBe(1);
+    expect(signInSpy).to.have.callCount(1);
   });
 
-  it('should call the Firebase sign in method and handle error', () => {
-    const dispatchMock = jest.fn();
-    const getStateMock = jest.fn();
+  it('should call the Firebase sign in method and handle error', async () => {
+    const dispatchMock = sinon.stub();
+    const getStateMock = sinon.stub();
 
-    signInSpy.mockRejectedValue(new Error('Some error'));
+    signInSpy.onFirstCall().rejects(new Error('Some error'));
 
     actions.signIn()(dispatchMock, getStateMock, undefined);
 
     // Checks that signInWithPopup() was called.
-    expect(signInSpy.mock.calls.length).toBe(1);
+    expect(signInSpy).to.have.callCount(1);
   });
 
 });
