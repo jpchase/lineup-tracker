60c32eebcaea5cc551adbbfbcb21382fe48aeef9
Jason Chase <jasonpchase@gmail.com>
2019-11-03 23:02:32 -0500

    Add more tests for game detail component

--- test/components/lineup-view-game-detail.test.ts
+++ test/components/lineup-view-game-detail.test.ts
@@ -1,16 +1,88 @@
+import { GET_GAME_SUCCESS } from '@app/actions/game-types';
 import { LineupViewGameDetail } from '@app/components/lineup-view-game-detail';
 import '@app/components/lineup-view-game-detail.js';
+import { GameDetail, GameStatus } from '@app/models/game';
+import { game } from '@app/reducers/game';
+import { resetState, store } from '@app/store';
 import { expect, fixture } from '@open-wc/testing';
+import { buildRoster, getNewGameWithLiveDetail, getStoredPlayer } from '../helpers/test_data';
+
+function getGameDetail(): GameDetail {
+  return getNewGameWithLiveDetail(buildRoster([getStoredPlayer()]));
+}
 
 describe('lineup-view-game-detail tests', () => {
   let el: LineupViewGameDetail;
   beforeEach(async () => {
+    store.addReducers({
+      game
+    });
+    // Resets to the initial store state.
+    store.dispatch(resetState());
+
     el = await fixture('<lineup-view-game-detail active></lineup-view-game-detail>');
   });
 
+  function getPlayerSection(mode: string): HTMLDivElement {
+    const section = el.shadowRoot!.querySelector(`#live-${mode}`);
+    expect(section, `Missing section for mode ${mode}`).to.be.ok;
+
+    return section as HTMLDivElement;
+  }
+
   it('shows no game placeholder when no current game', () => {
+    expect(store.getState().game).to.be.ok;
+    expect(store.getState().game!.game, 'GameState should have game unset').to.not.be.ok;
+
     const placeholder = el.shadowRoot!.querySelector('section p.empty-list');
     expect(placeholder, 'Missing empty placeholder element').to.be.ok;
+  });
+
+  it('shows starter player sections for new game', async () => {
+    const game = getGameDetail();
+    expect(game.status).to.equal(GameStatus.New);
+    store.dispatch({ type: GET_GAME_SUCCESS, game });
+    await el.updateComplete;
+
+    const starters = getPlayerSection('on');
+    const onHeader = starters.querySelector('h5');
+    expect(onHeader, 'Missing starters header').to.be.ok;
+    expect(onHeader!.textContent!.trim()).to.equal('Starters');
+
+    const offPlayers = getPlayerSection('off');
+    const offHeader = offPlayers.querySelector('h5');
+    expect(offHeader, 'Missing starters header').to.be.ok;
+    expect(offHeader!.textContent!.trim()).to.equal('Subs');
+
+    const nextPlayers = getPlayerSection('next');
+    expect(nextPlayers.hidden, 'Section for next should be hidden').to.be.true;
+
+    const outPlayers = getPlayerSection('out');
+    expect(outPlayers.hidden, 'Section for out should be hidden').to.be.true;
+  });
+
+  it('shows all player sections for started game', async () => {
+    const game = getGameDetail();
+    game.status = GameStatus.Start;
+
+    store.dispatch({ type: GET_GAME_SUCCESS, game });
+    await el.updateComplete;
+
+    const starters = getPlayerSection('on');
+    const onHeader = starters.querySelector('h5');
+    expect(onHeader, 'Missing starters header').to.be.ok;
+    expect(onHeader!.textContent!.trim()).to.equal('Starters');
+
+    const nextPlayers = getPlayerSection('next');
+    expect(nextPlayers.hidden, 'Section for next should be shown').to.be.false;
+
+    const offPlayers = getPlayerSection('off');
+    const offHeader = offPlayers.querySelector('h5');
+    expect(offHeader, 'Missing starters header').to.be.ok;
+    expect(offHeader!.textContent!.trim()).to.equal('Subs');
+
+    const outPlayers = getPlayerSection('out');
+    expect(outPlayers.hidden, 'Section for out should be shown').to.be.false;
   });
 
   it('a11y', async () => {
