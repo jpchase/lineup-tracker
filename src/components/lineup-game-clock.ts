f329382edce8225cb74007a2d1946ea2fe8b5557
jpchase <jasonpchase@gmail.com>
2020-11-15 21:33:51 -0500

    Fix mismatched end tag

--- src/components/lineup-game-clock.ts
+++ src/components/lineup-game-clock.ts
@@ -45,7 +45,7 @@ export class LineupGameClock extends LitElement {
           onIcon="pause_circle_outline" offIcon="play_circle_outline" label="Start/stop the clock"
           @MDCIconButtonToggle:change="${this._toggleClock}"></mwc-icon-button-toggle>
         <span id="periodTimer">${this.timerText}</span>&nbsp;[<span id="gameTimer"></span>]
-      </div>`
+      </span>`
   }
 
   private _timer?: Timer;
