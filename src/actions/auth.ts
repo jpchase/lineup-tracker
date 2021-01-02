5bf197f801f43a56cced3dd970e5b06e078cea52
jpchase <jasonpchase@gmail.com>
2021-01-02 10:23:30 -0500

    Sign in with test credentials for functional test
    - Add users for auth emulator to test data
    - Change existing test team and games to be owned by test user
    - Add test team and games for second user

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -3,20 +3,23 @@
 @license
 */
 
+import { User as FirebaseUser, UserCredential } from '@firebase/auth-types';
 import { Action, ActionCreator } from 'redux';
 import { ThunkAction } from 'redux-thunk';
-import { RootState } from '../store';
+import { debug } from '../common/debug';
+import { authRef, firebaseRef, provider } from "../firebase";
+import { useTestData } from '../init';
 import { User } from '../models/auth';
-import { User as FirebaseUser, UserCredential } from '@firebase/auth-types';
-import { authRef, provider } from "../firebase";
-
 import { GET_USER_SUCCESS } from "../slices/auth-types";
+import { RootState } from '../store';
 
 export interface AuthActionGetUser extends Action<typeof GET_USER_SUCCESS> { user: User };
 export type AuthAction = AuthActionGetUser;
 
 type ThunkResult = ThunkAction<void, RootState, undefined, AuthAction>;
 type ThunkPromise<R> = ThunkAction<Promise<R>, RootState, undefined, AuthAction>;
+
+const debugAuth = debug('auth');
 
 export const getUser: ActionCreator<ThunkPromise<boolean>> = () => (dispatch) => {
   // Return a promise that resolves once the auth state is known, via callback.
@@ -47,14 +50,26 @@ export const getUser: ActionCreator<ThunkPromise<boolean>> = () => (dispatch) =>
 };
 
 export const signIn: ActionCreator<ThunkResult> = () => () => {
-  authRef
-    .signInWithPopup(provider)
+  let signinPromise: Promise<UserCredential>;
+
+  if (useTestData()) {
+    debugAuth('Sign in with test credential')
+    const credential = firebaseRef.auth.GoogleAuthProvider.credential(
+      `{"sub": "3FK9P5Ledx4voK8w5Ep07DS6dCUc", "email": "algae.orange.312@test.com", "email_verified": true}`
+    )
+    signinPromise = authRef.signInWithCredential(credential);
+  } else {
+    debugAuth('Sign in with popup, as usual');
+    signinPromise = authRef.signInWithPopup(provider);
+  }
+  signinPromise
     .then((result: UserCredential) => {
       console.log(`Sign in succeeded: ${result.user ? result.user.uid : null}`);
     })
     .catch(error => {
       console.error(`Error trying to sign in: ${error}`);
-    });
+    })
+    .finally(() => { console.log('got to the finally clause') });
 };
 
 function getUserFromFirebaseUser(firebaseUser: FirebaseUser): User {
