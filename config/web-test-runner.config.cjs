604fa1804e7cbe39b59efc2f1c21f7648f6bb629
jpchase <jasonpchase@gmail.com>
2021-10-12 00:55:42 -0400

    Use web-test-runner for one test file

---
+++ config/web-test-runner.config.cjs
@@ -0,0 +1,41 @@
+const path = require('path');
+const puppeteerLauncher = require('@web/test-runner-puppeteer').puppeteerLauncher;
+
+function aliasResolverPlugin() {
+  return {
+    async resolveImport({ source, context }) {
+      if (!source.startsWith('@app/')) {
+        return;
+      }
+      const requestedFile = context.path.endsWith('/') ? `${context.path}index.html` : context.path;
+      const depth = requestedFile.split('/').length - 1;
+      let extension = path.extname(source);
+      if (!extension || extension.length === 0) {
+        // Default the extension, otherwise it won't resolve.
+        extension = '.js';
+      } else {
+        // Has an extension, which will already be included in the result.
+        extension = '';
+      }
+      const browserPath = `${'../'.repeat(depth - 1)}src/${source.substring(5)}${extension}`;
+      return browserPath;
+    }
+  };
+}
+
+const puppeteerExecutablePath = require('puppeteer').executablePath();
+
+module.exports = {
+  nodeResolve: true,
+  browsers: [
+    puppeteerLauncher({
+      launchOptions: {
+        executablePath: puppeteerExecutablePath,
+        // args: ['--some-flag'],
+      },
+    }),
+  ],
+  plugins: [
+    aliasResolverPlugin(),
+  ],
+};
