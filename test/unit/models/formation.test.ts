4ba17af9cf6c8bd2f2fcff952381b9fd2e1c7ae5
jpchase <jasonpchase@gmail.com>
2022-06-13 01:02:27 -0400

    Add 3-1-4-2 formation

--- test/unit/models/formation.test.ts
+++ test/unit/models/formation.test.ts
@@ -1,5 +1,5 @@
 import { expect } from '@open-wc/testing';
-import { Formation, FormationBuilder, FormationType} from '@app/models/formation';
+import { Formation, FormationBuilder, FormationType } from '@app/models/formation';
 
 describe('FormationBuilder', () => {
 
@@ -14,38 +14,38 @@ describe('FormationBuilder', () => {
       type: FormationType.F4_3_3,
       forward1: {
         id: 'FWD1',
-        positions: [{id: 'S', type: 'S'}]
+        positions: [{ id: 'S', type: 'S' }]
       },
       forward2: {
         id: 'FWD2',
         positions: [
-          {id: 'LW', type: 'W'},
-          {id: 'RW', type: 'W'}
+          { id: 'LW', type: 'W' },
+          { id: 'RW', type: 'W' }
         ]
       },
       midfield1: {
         id: 'MID1',
         positions: [
-          {id: 'AM1', type: 'AM'},
-          {id: 'AM2', type: 'AM'}
+          { id: 'AM1', type: 'AM' },
+          { id: 'AM2', type: 'AM' }
         ]
       },
       midfield2: {
         id: 'MID2',
-        positions: [{id: 'HM', type: 'HM'}]
+        positions: [{ id: 'HM', type: 'HM' }]
       },
       defense: {
         id: 'DEF',
         positions: [
-          {id: 'LFB', type: 'FB'},
-          {id: 'LCB', type: 'CB'},
-          {id: 'RCB', type: 'CB'},
-          {id: 'RFB', type: 'FB'},
+          { id: 'LFB', type: 'FB' },
+          { id: 'LCB', type: 'CB' },
+          { id: 'RCB', type: 'CB' },
+          { id: 'RFB', type: 'FB' },
         ]
       },
       gk: {
         id: 'GK',
-        positions: [{id: 'GK', type: 'GK'}]
+        positions: [{ id: 'GK', type: 'GK' }]
       },
     };
 
@@ -54,4 +54,49 @@ describe('FormationBuilder', () => {
     expect(new4_3_3).to.deep.equal(expectedFormation);
   });
 
+  it('create should handle 3-1-4-2', () => {
+    const expectedFormation: Formation = {
+      type: FormationType.F3_1_4_2,
+      forward1: {
+        id: 'FWD1',
+        positions: [
+          { id: 'S1', type: 'S' },
+          { id: 'S2', type: 'S' }
+        ]
+      },
+      forward2: {
+        id: 'FWD2',
+        positions: []
+      },
+      midfield1: {
+        id: 'MID1',
+        positions: [
+          { id: 'LW', type: 'W' },
+          { id: 'AM1', type: 'AM' },
+          { id: 'AM2', type: 'AM' },
+          { id: 'RW', type: 'W' }
+        ]
+      },
+      midfield2: {
+        id: 'MID2',
+        positions: [{ id: 'HM', type: 'HM' }]
+      },
+      defense: {
+        id: 'DEF',
+        positions: [
+          { id: 'LFB', type: 'FB' },
+          { id: 'CB', type: 'CB' },
+          { id: 'RFB', type: 'FB' },
+        ]
+      },
+      gk: {
+        id: 'GK',
+        positions: [{ id: 'GK', type: 'GK' }]
+      },
+    };
+
+    const new3_1_4_2: Formation = FormationBuilder.create(FormationType.F3_1_4_2);
+
+    expect(new3_1_4_2).to.deep.equal(expectedFormation);
+  });
 });
