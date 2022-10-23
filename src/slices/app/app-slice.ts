3f7d620dc9f5e131b0c487573e48f002aba7954c
jpchase <jasonpchase@gmail.com>
2022-10-23 02:11:46 -0400

    Move app action creators into slice file

--- src/slices/app/app-slice.ts
+++ src/slices/app/app-slice.ts
@@ -1,4 +1,82 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AnyAction, createSlice, PayloadAction, ThunkAction } from '@reduxjs/toolkit';
+import { getEnv } from '../../app/environment.js';
+import { RootState } from '../../store.js';
+
+const env = getEnv();
+
+type ThunkResult = ThunkAction<void, RootState, undefined, AnyAction>;
+
+export const navigate = (location: Location): ThunkResult => (dispatch) => {
+  // Extract the page name from path.
+  const pathname = location.pathname;
+  const parts = pathname.slice(1).split('/');
+  let page = parts[0] || 'viewHome';
+
+  console.log(`navigate: got page = ${page} from location`, location.href);
+  // Game views have path: /{view}/{gameId}
+  const gameId = parts[1];
+
+  dispatch(loadPage(page, gameId));
+
+  // Close the drawer - in case the *path* change came from a link in the drawer.
+  dispatch(updateDrawerState(false));
+};
+
+const loadPage = (page: string, gameId: string): ThunkResult => async (dispatch) => {
+  console.log(`loadPage: page = ${page}, gameId = ${gameId}`);
+  switch (page) {
+    case 'viewHome':
+      import('../../components/lineup-view-home').then(() => {
+        // Put code in here that you want to run every time when
+        // navigating to viewHome after lineup-view-home.js is loaded.
+      });
+      break;
+    case 'viewGames':
+      import('../../components/lineup-view-games');
+      break;
+    case 'game':
+      const detailModule = await import('../../components/lineup-view-game-detail');
+      // Fetch the data for the given game id.
+      console.log(`loading game detail page for ${gameId}`);
+      await dispatch(detailModule.getGame(gameId));
+      break;
+    case 'gameroster':
+      const rosterModule = await import('../../components/lineup-view-game-roster');
+      // Fetch the data for the given game id.
+      console.log(`loading game roster page for ${gameId}`);
+      await dispatch(rosterModule.getGame(gameId));
+      break;
+    case 'viewRoster':
+      import('../../components/lineup-view-roster');
+      break;
+    case 'addNewTeam':
+      import('../../components/lineup-view-team-create');
+      break;
+    default:
+      page = 'view404';
+      import('../../components/lineup-view404');
+  }
+
+  dispatch(updatePage(page));
+};
+
+let snackbarTimer: number;
+
+export const showSnackbar = (): ThunkResult => (dispatch) => {
+  dispatch(openSnackBar());
+  window.clearTimeout(snackbarTimer);
+  snackbarTimer = window.setTimeout(() =>
+    dispatch(closeSnackBar()), 3000);
+};
+
+export const offlineChanged = (offline: boolean): ThunkResult => (dispatch, getState) => {
+  const appState = getState().app!;
+  // Show the snackbar only if offline status changes.
+  if (offline !== appState.offline && !env.disableOfflineDetection) {
+    dispatch(showSnackbar());
+  }
+  dispatch(updateOffline(offline));
+};
 
 export interface AppState {
   page: string;
