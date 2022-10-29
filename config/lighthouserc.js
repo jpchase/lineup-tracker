4ebbe50b4d71310df3590b3430a7d8894f18be8b
jpchase <jasonpchase@gmail.com>
2022-10-29 16:24:22 -0400

    Fix url and storage reset

--- config/lighthouserc.js
+++ config/lighthouserc.js
@@ -4,7 +4,7 @@ module.exports = {
       puppeteerScript: 'config/lighthouse-ci-init.js',
       startServerCommand: 'npm run serve:test',
       url: [
-        'http://127.0.0.1:8791/viewRoster?team=test_team1'
+        'http://localhost:8791/viewRoster?team=test_team1'
       ]
     },
     upload: {
