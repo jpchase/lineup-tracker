63441b00d821939a5cef72f24dff567012ac649b
jpchase <jasonpchase@gmail.com>
2022-10-30 00:55:03 -0400

    Longer timeout for starting server

--- config/lighthouserc.js
+++ config/lighthouserc.js
@@ -3,10 +3,15 @@ module.exports = {
     collect: {
       puppeteerScript: 'config/lighthouse-ci-init.js',
       startServerCommand: 'npm run serve:test',
+      startServerReadyTimeout: 90000,
+      settings: { disableStorageReset: true },
       url: [
         'http://localhost:8791/viewRoster?team=test_team1'
       ]
     },
+    // assert: {
+    //   preset: 'lighthouse:recommended',
+    // },
     upload: {
       target: 'temporary-public-storage',
     },
