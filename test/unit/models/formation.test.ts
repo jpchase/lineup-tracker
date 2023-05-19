909ed4edb8b2bc96c7ad655e176ab79a1aeed6d6
Jason Chase <jasonpchase@gmail.com>
2023-05-19 19:38:59 -0400

    Remove override for the "camelcase" rule

--- test/unit/models/formation.test.ts
+++ test/unit/models/formation.test.ts
@@ -1,8 +1,9 @@
+/** @format */
+
 import { expect } from '@open-wc/testing';
 import { Formation, FormationBuilder, FormationType } from '@app/models/formation';
 
 describe('FormationBuilder', () => {
-
   it('create should throw for unknown formation type', () => {
     expect(() => {
       FormationBuilder.create(<FormationType>'no such formation');
@@ -14,25 +15,25 @@ describe('FormationBuilder', () => {
       type: FormationType.F4_3_3,
       forward1: {
         id: 'FWD1',
-        positions: [{ id: 'S', type: 'S' }]
+        positions: [{ id: 'S', type: 'S' }],
       },
       forward2: {
         id: 'FWD2',
         positions: [
           { id: 'LW', type: 'W' },
-          { id: 'RW', type: 'W' }
-        ]
+          { id: 'RW', type: 'W' },
+        ],
       },
       midfield1: {
         id: 'MID1',
         positions: [
           { id: 'AM1', type: 'AM' },
-          { id: 'AM2', type: 'AM' }
-        ]
+          { id: 'AM2', type: 'AM' },
+        ],
       },
       midfield2: {
         id: 'MID2',
-        positions: [{ id: 'HM', type: 'HM' }]
+        positions: [{ id: 'HM', type: 'HM' }],
       },
       defense: {
         id: 'DEF',
@@ -41,17 +42,17 @@ describe('FormationBuilder', () => {
           { id: 'LCB', type: 'CB' },
           { id: 'RCB', type: 'CB' },
           { id: 'RFB', type: 'FB' },
-        ]
+        ],
       },
       gk: {
         id: 'GK',
-        positions: [{ id: 'GK', type: 'GK' }]
+        positions: [{ id: 'GK', type: 'GK' }],
       },
     };
 
-    const new4_3_3: Formation = FormationBuilder.create(FormationType.F4_3_3);
+    const new433 = FormationBuilder.create(FormationType.F4_3_3);
 
-    expect(new4_3_3).to.deep.equal(expectedFormation);
+    expect(new433).to.deep.equal(expectedFormation);
   });
 
   it('create should handle 4-2-3-1', () => {
@@ -59,11 +60,11 @@ describe('FormationBuilder', () => {
       type: FormationType.F4_2_3_1,
       forward1: {
         id: 'FWD1',
-        positions: [{ id: 'S', type: 'S' }]
+        positions: [{ id: 'S', type: 'S' }],
       },
       forward2: {
         id: 'FWD2',
-        positions: []
+        positions: [],
       },
       midfield1: {
         id: 'MID1',
@@ -71,14 +72,14 @@ describe('FormationBuilder', () => {
           { id: 'LW', type: 'W' },
           { id: 'AM', type: 'AM' },
           { id: 'RW', type: 'W' },
-        ]
+        ],
       },
       midfield2: {
         id: 'MID2',
         positions: [
           { id: 'HM1', type: 'HM' },
-          { id: 'HM2', type: 'HM' }
-        ]
+          { id: 'HM2', type: 'HM' },
+        ],
       },
       defense: {
         id: 'DEF',
@@ -87,17 +88,17 @@ describe('FormationBuilder', () => {
           { id: 'LCB', type: 'CB' },
           { id: 'RCB', type: 'CB' },
           { id: 'RFB', type: 'FB' },
-        ]
+        ],
       },
       gk: {
         id: 'GK',
-        positions: [{ id: 'GK', type: 'GK' }]
+        positions: [{ id: 'GK', type: 'GK' }],
       },
     };
 
-    const new4_2_3_1: Formation = FormationBuilder.create(FormationType.F4_2_3_1);
+    const new4231 = FormationBuilder.create(FormationType.F4_2_3_1);
 
-    expect(new4_2_3_1).to.deep.equal(expectedFormation);
+    expect(new4231).to.deep.equal(expectedFormation);
   });
 
   it('create should handle 3-1-4-2', () => {
@@ -107,12 +108,12 @@ describe('FormationBuilder', () => {
         id: 'FWD1',
         positions: [
           { id: 'S1', type: 'S' },
-          { id: 'S2', type: 'S' }
-        ]
+          { id: 'S2', type: 'S' },
+        ],
       },
       forward2: {
         id: 'FWD2',
-        positions: []
+        positions: [],
       },
       midfield1: {
         id: 'MID1',
@@ -120,12 +121,12 @@ describe('FormationBuilder', () => {
           { id: 'LW', type: 'W' },
           { id: 'AM1', type: 'AM' },
           { id: 'AM2', type: 'AM' },
-          { id: 'RW', type: 'W' }
-        ]
+          { id: 'RW', type: 'W' },
+        ],
       },
       midfield2: {
         id: 'MID2',
-        positions: [{ id: 'HM', type: 'HM' }]
+        positions: [{ id: 'HM', type: 'HM' }],
       },
       defense: {
         id: 'DEF',
@@ -133,16 +134,16 @@ describe('FormationBuilder', () => {
           { id: 'LFB', type: 'FB' },
           { id: 'CB', type: 'CB' },
           { id: 'RFB', type: 'FB' },
-        ]
+        ],
       },
       gk: {
         id: 'GK',
-        positions: [{ id: 'GK', type: 'GK' }]
+        positions: [{ id: 'GK', type: 'GK' }],
       },
     };
 
-    const new3_1_4_2: Formation = FormationBuilder.create(FormationType.F3_1_4_2);
+    const new3142 = FormationBuilder.create(FormationType.F3_1_4_2);
 
-    expect(new3_1_4_2).to.deep.equal(expectedFormation);
+    expect(new3142).to.deep.equal(expectedFormation);
   });
 });
