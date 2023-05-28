9ec23dc4132e4903f60eacfa6975af4621469386
Jason Chase <jasonpchase@gmail.com>
2023-05-28 13:46:37 -0400

    Format files

--- src/components/lineup-on-player-list.ts
+++ src/components/lineup-on-player-list.ts
@@ -1,3 +1,5 @@
+/** @format */
+
 import '@material/mwc-button';
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
@@ -16,11 +18,13 @@ interface PlayerLine extends FormationLine {
 }
 
 function getLineForPosition(lines: PlayerLine[], position: Position): PlayerLine | undefined {
-  return lines.find(line => line.positions.some(linePosition => (linePosition.id === position.id)));
+  return lines.find((line) =>
+    line.positions.some((linePosition) => linePosition.id === position.id)
+  );
 }
 
 function getOpenPositionInLine(line: PlayerLine, position: Position): PlayerCardData | undefined {
-  return line.playerPositions.find(data => (!data.player && data.position.id === position.id));
+  return line.playerPositions.find((data) => !data.player && data.position.id === position.id);
 }
 
 // This element is *not* connected to the Redux store.
@@ -28,10 +32,11 @@ function getOpenPositionInLine(line: PlayerLine, position: Position): PlayerCard
 export class LineupOnPlayerList extends PlayerListElement {
   override render() {
     const lines = this._getPlayerLines();
-    return html`
-      ${SharedStyles}
+    return html` ${SharedStyles}
       <style>
-        :host { display: block; }
+        :host {
+          display: block;
+        }
 
         .line {
           border: 1px;
@@ -47,25 +52,33 @@ export class LineupOnPlayerList extends PlayerListElement {
         }
       </style>
       <div>
-      ${lines.length > 0 ? html`
-        <div class="list">
-        ${repeat(lines, (line: PlayerLine) => line.id, (line: PlayerLine /*, index: number*/) => html`
-          <div class="line">
-          ${repeat(line.playerPositions, (cardData: PlayerCardData) => cardData.id, (cardData: PlayerCardData /*, index: number*/) => html`
-            <lineup-player-card mode="${PlayerStatus.On}"
-                                .data="${cardData}"
-                                .timeTracker="${this.getTracker(cardData.player!)}">
-            </lineup-player-card>
-          `)}
-          </div>
-        `)}
-        </div>
-      ` : html`
-        <p class="empty-list">
-          Formation not set.
-        </p>
-      `}
-      </div>`
+        ${lines.length > 0
+          ? html`
+              <div class="list">
+                ${repeat(
+                  lines,
+                  (line: PlayerLine) => line.id,
+                  (line: PlayerLine /*, index: number*/) => html`
+                    <div class="line">
+                      ${repeat(
+                        line.playerPositions,
+                        (cardData: PlayerCardData) => cardData.id,
+                        (cardData: PlayerCardData /*, index: number*/) => html`
+                          <lineup-player-card
+                            mode="${PlayerStatus.On}"
+                            .data="${cardData}"
+                            .timeTracker="${this.getTracker(cardData.player!)}"
+                          >
+                          </lineup-player-card>
+                        `
+                      )}
+                    </div>
+                  `
+                )}
+              </div>
+            `
+          : html` <p class="empty-list">Formation not set.</p> `}
+      </div>`;
   }
 
   @property({ type: Array })
@@ -86,22 +99,24 @@ export class LineupOnPlayerList extends PlayerListElement {
     const formation = this.formation;
     const selectedPositionId = this.selectedPosition ? this.selectedPosition.id : null;
     const lines: PlayerLine[] = [
-      formation.forward1, formation.forward2,
-      formation.midfield1, formation.midfield2,
+      formation.forward1,
+      formation.forward2,
+      formation.midfield1,
+      formation.midfield2,
       formation.defense,
-      formation.gk
+      formation.gk,
     ].reduce((result: PlayerLine[], formationLine) => {
       const line = {
         id: formationLine.id,
         positions: formationLine.positions,
-        playerPositions: <PlayerCardData[]>[]
+        playerPositions: <PlayerCardData[]>[],
       };
       // Creates placeholders for each position in the formation.
-      formationLine.positions.forEach(position => {
+      formationLine.positions.forEach((position) => {
         const selected = position.id === selectedPositionId;
         line.playerPositions.push({
           id: line.id + position.id,
-          position: { ...position, selected }
+          position: { ...position, selected },
         });
       });
       result.push(line);
--- src/components/shared-styles.ts
+++ src/components/shared-styles.ts
@@ -1,87 +1,79 @@
-/**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
+/** @format */
 
 import { html } from 'lit';
 
 export const SharedStyles = html`
-<style>
-  :host {
-    display: block;
-    box-sizing: border-box;
-  }
-
-  [hidden] {
-    display: none !important;
-  }
-
-  section {
-    padding: 24px;
-    background: var(--app-section-odd-color);
-  }
-
-  section > * {
-    max-width: 600px;
-    margin-right: auto;
-    margin-left: auto;
-  }
-
-  section:nth-of-type(even) {
-    background: var(--app-section-even-color);
-  }
-
-  h2 {
-    font-size: 24px;
-    text-align: center;
-    color: var(--app-dark-text-color);
-  }
-
-  @media (min-width: 460px) {
-    h2 {
-      font-size: 36px;
+  <style>
+    :host {
+      display: block;
+      box-sizing: border-box;
     }
-  }
 
-  /* Temp to make h3 look like h5, to avoid screenshot changes */
-  h3.h5 {
-    display: block;
-    font-size: 0.83em;
-    margin-block-start: 1.67em;
-    margin-block-end: 1.67em;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
-    font-weight: bold;
-  }
+    [hidden] {
+      display: none !important;
+    }
 
-  .empty-list {
-    text-align: center;
-    white-space: nowrap;
-    /* color: var(--app-secondary-color); */
-  }
+    section {
+      padding: 24px;
+      background: var(--app-section-odd-color);
+    }
 
-  .flex-equal-justified {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-  }
+    section > * {
+      max-width: 600px;
+      margin-right: auto;
+      margin-left: auto;
+    }
 
-  .circle {
-    display: block;
-    width: 64px;
-    height: 64px;
-    margin: 0 auto;
-    text-align: center;
-    border-radius: 50%;
-    background: var(--app-primary-color);
-    color: var(--app-light-text-color);
-    font-size: 30px;
-    line-height: 64px;
-  }
-</style>
+    section:nth-of-type(even) {
+      background: var(--app-section-even-color);
+    }
+
+    h2 {
+      font-size: 24px;
+      text-align: center;
+      color: var(--app-dark-text-color);
+    }
+
+    @media (min-width: 460px) {
+      h2 {
+        font-size: 36px;
+      }
+    }
+
+    /* Temp to make h3 look like h5, to avoid screenshot changes */
+    h3.h5 {
+      display: block;
+      font-size: 0.83em;
+      margin-block-start: 1.67em;
+      margin-block-end: 1.67em;
+      margin-inline-start: 0px;
+      margin-inline-end: 0px;
+      font-weight: bold;
+    }
+
+    .empty-list {
+      text-align: center;
+      white-space: nowrap;
+      /* color: var(--app-secondary-color); */
+    }
+
+    .flex-equal-justified {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+
+    .circle {
+      display: block;
+      width: 64px;
+      height: 64px;
+      margin: 0 auto;
+      text-align: center;
+      border-radius: 50%;
+      background: var(--app-primary-color);
+      color: var(--app-light-text-color);
+      font-size: 30px;
+      line-height: 64px;
+    }
+  </style>
 `;
