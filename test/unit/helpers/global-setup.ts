f9700aadf3ca1d68ea1cdb6c2699cc514be51ee4
jpchase <jasonpchase@gmail.com>
2026-01-04 19:47:26 -0500

    Workaround for timeouts in accessibility checks
    After instrumenting the `chai-a11y-axe` code, the problem seems to be with the
    dynamic import [1] of the `axe-core` package. When the timeouts occur in a test,
    it looks like the import is called but the promise doesn't complete before the
    test times out. This can happen if there are multiple tests that use the
    `accessible` expect helper.
    
    As a workaround, use the global setup script to statically import `axe-core`
    before any tests run. This seems to ensure that `window.axe` is set, and then
    the dynamic import isn't needed.
    
    [1] https://github.com/open-wc/open-wc/blob/5aeaf35e01a2f15b6663d71102eeea3333e4c57d/packages/chai-a11y-axe/src/axe-import.js#L25

--- test/unit/helpers/global-setup.ts
+++ test/unit/helpers/global-setup.ts
@@ -1,4 +1,10 @@
 /** @format */
+// Import in advance to avoid timeouts using the `accessible` helper. Timeouts
+// can occur because the `accessible` helper attempts to dynamically import the
+// 'axe-core', which can happen concurrently during test execution. In theory,
+// the concurrent imports should work fine, but it seems to cause random timeouts
+// when an import doesn't complete.
+import 'axe-core/axe.min.js';
 
 // WORKAROUND for immer.js esm (see https://github.com/immerjs/immer/issues/557)
 if (typeof window === 'object') {
